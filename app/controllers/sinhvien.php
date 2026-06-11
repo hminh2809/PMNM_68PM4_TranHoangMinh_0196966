78fb44bc9f310ad3776de81600b19b8d34d4413c
hminh2809 <0196966@st.huce.edu.vn>
2026-06-11 14:35:36 +0700

    feat<sinhvien>: delete sinhvien

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -2,10 +2,12 @@
 require_once '../app/core/controller.php';
 class sinhvien extends Controller{
 
-    public function index(){
+    public function index($limit = 5, $offset = 0, $search=""){
         $sinhvienModel = $this->model('sinhvienModel');
-        $sinhviens = $sinhvienModel->getAllSinhvien();
-        $this->view('layout/masterlayout', ['viewname' => 'sinhvien/index', 'sinhviens' => $sinhviens, 'title' => 'Danh sách sinh viên']);
+        $result = $sinhvienModel->paging($limit, $offset, $search);
+        $sinhviens = $result['sinhviens'];
+        $totalpage = $result['totalpage'];
+        $this->view('layout/masterlayout', ['viewname' => 'sinhvien/index', 'sinhviens' => $sinhviens, 'title' => 'Danh sách sinh viên', 'totalpage'=>$totalpage]);
     }
 
     public function create(){
@@ -24,6 +26,37 @@ class sinhvien extends Controller{
             }else{
                 echo "Thêm mới thất bại";
             }
+        }
+    }
+
+    public function edit($id){
+        $sinhvienModel = $this->model('sinhvienModel');
+        $sinhvien = $sinhvienModel->getById($id);
+        $this->view('sinhvien/edit', ['sinhvien' => $sinhvien]);
+    }
+
+    public function update($id){
+        if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST'){
+            $hoten = $_POST['hoten'] ??'';
+            $gioitinh = $_POST['gioitinh'] ??'';
+            $mssv = $_POST['mssv'] ??'';
+            $sinhvienModel = $this->model('sinhvienModel');
+            $result = $sinhvienModel->update($id, $hoten, $gioitinh, $mssv);
+            if($result){
+                echo "Cập nhật thành công";
+            }else{
+                echo "Cập nhật thất bại";
+            }
+        }
+    }
+
+    public function delete($id){
+        $sinhvienModel = $this->model('sinhvienModel');
+        $result = $sinhvienModel->delete($id);
+        if($result){
+            echo "Xóa thành công";
+        }else{
+            echo "Xóa thất bại";
         }
     }
 }

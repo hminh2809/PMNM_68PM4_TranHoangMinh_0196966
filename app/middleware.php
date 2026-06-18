c5edd151ac37cf1a57cbfef804eb1cfdc4985998
hminh2809 <0196966@st.huce.edu.vn>
2026-06-19 00:19:40 +0700

    fix

---
+++ app/middleware.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../app/core/App.php';
+    class middleware {
+        function checklogin() {
+            $publicPages = [
+                '/PMNM_68PM4_TranHoangMinh_0196966/public/home/login',
+                '/PMNM_68PM4_TranHoangMinh_0196966/public/auth/login'
+            ];
+            if(!isset($_SESSION['username']) && !in_array($_SERVER['REQUEST_URI'], $publicPages)) {
+                header('Location: /PMNM_68PM4_TranHoangMinh_0196966/public/home/login');
+                exit();
+            }
+        }
+    }
+?>
+
+
